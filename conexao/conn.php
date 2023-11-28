b7d459a58fc9c0e9367631789cc9658d86509da1
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2023-11-28 11:38:17 -0300

    Adicionado Arquivo conexão ao BD

---
+++ conexao/conn.php
@@ -0,0 +1,18 @@
+<?php
+include_once("env.php");
+
+date_default_timezone_set('America/Sao_Paulo');
+
+$dir= "../logs/logs.log";
+try{
+	
+	$pdo = new PDO("mysql:dbname=$banco;host=$host;charset=utf8", "$user", "$senha");
+        $mensagem_log = "LOG: Conectado ao Banco de Dados " . date("Y-m-d H:i:s") . PHP_EOL;
+    } catch (Exception $e) {
+	    $mensagem_log = "LOG: Conexão mal sucedida - " . $e->getMessage() . " " . date("Y-m-d H:i:s") . PHP_EOL;
+} 
+
+	file_put_contents($dir, $mensagem_log, FILE_APPEND);
+?>
+
+
