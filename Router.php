2698e18864369d4c3e6010292a27ffad1acb2e5f
sidneyojr <sidney.o@aluno.ifsp.edu.br>
2024-01-11 15:40:53 -0300

    Rotas 2

--- Router.php
+++ Router.php
@@ -1,6 +1,6 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+#ini_set('display_errors', 1);
+#error_reporting(E_ALL);
 date_default_timezone_set('America/Sao_Paulo');
 
 require_once __DIR__ . '/conexao/conn.php';
